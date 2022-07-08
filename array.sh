7707efa74854d7b04ec560d79e83959a10bcf069
Aleksey Pivchenkov <aleksey_pivchenkov@epam.com>
2022-07-08 10:18:11 +0000

    Ready

--- array.sh
+++ array.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
 # Place your code here
+i=0
+
+if [[ $1 -eq 1 ]]
+then
+    folder_word="folder"
+else
+    folder_word="folders"
+fi
+
+echo -n "$1 $folder_word created: "
+
+for letter in {a..z}
+do
+    if [[ i -eq $1 ]]
+    then
+        break
+    else
+
+        i=$((i+1))
+    fi
+
+    mkdir "folder_$letter"
+    echo -n "folder_$letter"
+    if [[ i -lt $1 ]]
+    then
+        echo -n ", "
+    else
+        echo
+    fi
+done
